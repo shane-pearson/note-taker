e690ecf2f2c2c33fb80f3568879b788c7848000d
Pearson <spearson@lwsd.org>
2023-01-31 18:21:18 -0800

    added changes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-const uuid = require('./helpers/uuid');
+const uuid = require('uuid');
+const notes = require('./db/db.json');
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-app.get('/', (req, res) =>
+app.get('/notes/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
@@ -23,5 +24,15 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.get('/api/notes', (req, res) =>
+  res.json(notes)
+);
+
+app.post('/api/notes', (req, res) => {
+    notes.push(req.body) 
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.json(notes)
+  }
+), 
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
